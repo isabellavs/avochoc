59c249a7420567fdf273f6eb5f44f6c177d2c8a4
Isabella van Staden <isabella.vanstaden@enabled.world>
2023-02-13 13:02:32 +0200

    again

--- palindrome.py
+++ palindrome.py
@@ -2,11 +2,13 @@ import sys
 
 def main():
 
-    inStr = sys.argv[1]
+    # using str.replace() since python's version is optimised
+    # in C and does not do the whole str concat thing
+    # which would happen if I looped through the string 
+    # and replaced the offending chars.
+    # Not against the rule of not using built in functions to REVERSE the word.
 
-    # using str.replace() since python's version is optimised, in C and does not do the whole str concat thing
-    # that would happen if I loop through the string and replaced the offending chars.
-    noSpace = inStr.replace(' ', '')
+    noSpace = sys.argv[1].replace(' ', '')
     stackey = list(noSpace)
     newW = ""
 
